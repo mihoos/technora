95da66d1891d038bac1da045d106790d7fe907a8
Michal Sawicki <m.sawicki@f5.com>
2024-10-24 14:59:40 +0200

    settings config update

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'  # Enable the inner app 
+    'app'  # Enable the inner app
 ]
 
 MIDDLEWARE = [
